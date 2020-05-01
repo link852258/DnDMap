2504cc5eefc606a1802891a70c67a4f47d4cfc9a
Maxime <maximemoring@hotmail.com>
2020-05-01 04:16:01 -0400

    Meeting #2.1

--- src/Block.java
+++ src/Block.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 import java.util.HashMap;
 
 public abstract class Block {
@@ -11,7 +10,7 @@ public abstract class Block {
     protected final int MAX_DOORS;
 
     // Variables
-    char[][] dataTable;
+    public char[][] dataTable;
     HashMap<String,Integer> sizesFromOriginalPos;
     int[] posTopLeft;
     Door[] doors;
@@ -41,20 +40,54 @@ public abstract class Block {
         // Set original position
         this.posTopLeft[0] = 201;
         this.posTopLeft[1] = 200 + ((height/2) + 1); // TO TEST POSITION
-        // TODO - Generate doors
         // Generate it's first entry door on [200, 200]
         doors[0] = new Door(200, 200, "left");
+        // Generate other doors
         int doorNb = doors.length;
-        for (int x = 1; x < doorNb; x++) {
-            doors[x] = new Door(true, this.posTopLeft[0], this.posTopLeft[1], width, height);
+        for (int X = 1; X < doorNb; X++) {
+            doors[X] = new Door(true, this.posTopLeft[0], this.posTopLeft[1], width, height);
         }
+        // Bake Block into its own dataTable
+        this.bakeIntoDataTable();
     }
 
     public void generate(int X, int Y, String direction, HashMap<String,Integer> space) {
         // TODO - Do after default generation
+        // Calculate available space
+        int availableWidth = space.get("left") + space.get("right");
+        if (availableWidth > MAX_WIDTH)
+            availableWidth = MAX_WIDTH;
+        int availableHeight = space.get("up") + space.get("down");
+        if (availableHeight > MAX_HEIGHT)
+            availableHeight = MAX_HEIGHT;
+        // Randomize sizes
+        int randWidth = randomInt(MIN_WIDTH, availableWidth);
+        int randHeight = randomInt(MIN_HEIGHT, availableHeight);
+        // Randomize position of originating door
+        int originDoorPos;
+        int minimum;
+        int maximum;
+        // SEUL JO CONNAITRA L'EFFORT QUI A ÉTÉ NÉCÉSSAIRE À CRÉER CECI
+        // Parse origin-door position minimum and maximum to avoid spillage
+        if (direction == "up" || direction == "down") {
+            if (randWidth > space.get("right"))
+                minimum = 1 + (randWidth - space.get("right")); // Minimum is the unavailable space
+            else
+                minimum = 1; // Maximum space to the right
+            if (randWidth > space.get("left"))
+                maximum = randWidth - (randWidth - space.get("left")); // Maximum is random width minus the unavailable place
+            else
+                maximum = randWidth;
+            originDoorPos = randomInt(minimum, maximum);
+        }
+        this.posTopLeft[0] = 5;
     }
 
-    public void bakeIntoDataTable(Block b) {
-        // TODO - Apply to the Block's dataTable
+    public void bakeIntoDataTable() {
+        for (int X = 0; X < width; X++) {
+            for (int Y = 0; Y < height; Y++) {
+                this.dataTable[this.posTopLeft[0]][this.posTopLeft[1]] = 'F';
+            }
+        }
     }
 }
--- src/Door.java
+++ src/Door.java
@@ -2,6 +2,7 @@ public class Door {
     boolean isLinked = false;
     String direction;
     int[] pos = new int[2];
+    int[] nextRoomPos = new int[2];
 
     // Constructor with exact position and direction
     Door(int X, int Y, String direction) {
@@ -12,7 +13,7 @@ public class Door {
     }
 
     // Constructor for randomized generation on Block
-    Door(boolean isFirstRoom, int X, int Y, int length, int height) {
+    Door(boolean isFirstRoom, int X, int Y, int width, int height) {
         // Randomize on which face the door is
         double rand = Math.random();
 
@@ -21,15 +22,41 @@ public class Door {
             rand = Math.random();
         }
 
+        int rand2;
         // Generate the door on the Block's face
         if (rand <= 0.25) { // left
-            direction = "left";
+            this.direction = "left";
+            rand2 = randomInt(0, height); // Takes a random position on the Block's left face
+            this.pos[0] = X - 1; // One before Block floor
+            this.pos[1] = Y + rand2;
+            this.nextRoomPos[0] = X - 2;
+            this.nextRoomPos[1] = Y + rand2;
         } else if (rand <= 0.5) { // up
             direction = "up";
+            rand2 = randomInt(0, width);
+            this.pos[0] = X + rand2;
+            this.pos[1] = Y - 1;
+            this.nextRoomPos[0] = X + rand2;
+            this.nextRoomPos[1] = Y - 2;
         } else if (rand <= 0.75) { // right
             direction = "right";
+            rand2 = randomInt(0, height);
+            this.pos[0] = X + width + 1;
+            this.pos[1] = Y + rand2;
+            this.nextRoomPos[0] = X + width + 2;
+            this.nextRoomPos[1] = Y + rand2;
         } else { // down
             direction = "down";
+            rand2 = randomInt(0, width);
+            this.pos[0] = X + rand2;
+            this.pos[1] = Y + height + 1;
+            this.nextRoomPos[0] = X + rand2;
+            this.nextRoomPos[1] = Y + height + 2;
         }
+    }
+
+    // Functions
+    public int randomInt(int min, int max) {
+        return (int)(Math.random() * (max - min + 1)) + min;
     }
 }
