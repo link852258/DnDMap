3c782c640b6d9a5cbeaef77b94e7de0b3bbf79eb
jonathan <link852258>
2020-05-05 01:01:53 -0400

    Refactoriser

--- src/Dnd.java
+++ src/Dnd.java
@@ -6,7 +6,7 @@ import java.awt.*;
 public class Dnd {
     public static void main(String[] args){
         MainFrame main = new MainFrame(1000,1000);
-        Dungeon d = new Dungeon(50,100,10);
+        Dungeon d = new Dungeon(50,100,8);
         d.generateDungeon();
         main.addImage(d.draw());
         main.show();
--- src/Door.java
+++ src/Door.java
@@ -1,3 +1,5 @@
+import java.util.stream.Stream;
+
 public class Door {
     boolean isLinked;
     String direction;
@@ -28,38 +30,30 @@ public class Door {
         int rand2;
         // Generate the door on the Block's face
         if (rand <= 0.25) { // left
-            this.direction = "left";
             rand2 = randomInt(0, height - 1); // Takes a random position on the Block's left face
-            this.posX = X - 1; // One before Block floor
-            this.posY = Y + rand2;
-            this.nextRoomPosX = X - 2;
-            this.nextRoomPosY = Y + rand2;
+            setDoor("left",X - 1,Y + rand2, X - 2, Y + rand2);
         } else if (rand <= 0.5) { // up
-            direction = "up";
             rand2 = randomInt(0, width - 1);
-            this.posX = X + rand2;
-            this.posY = Y - 1;
-            this.nextRoomPosX = X + rand2;
-            this.nextRoomPosY = Y - 2;
+            setDoor("up",X + rand2,Y - 1, X + rand2, Y - 2);
         } else if (rand <= 0.75) { // right
-            direction = "right";
             rand2 = randomInt(0, height - 1);
-            this.posX = X + width;
-            this.posY = Y + rand2;
-            this.nextRoomPosX = X + width + 1;
-            this.nextRoomPosY = Y + rand2;
+            setDoor("right",X + width,Y + rand2, X + width + 1, Y + rand2);
         } else { // down
-            direction = "down";
             rand2 = randomInt(0, width - 1);
-            this.posX = X + rand2;
-            this.posY = Y + height;
-            this.nextRoomPosX = X + rand2;
-            this.nextRoomPosY = Y + height + 1;
+            setDoor("down",X + rand2,Y + height, X + rand2, Y + height + 1);
         }
     }
 
     // Functions
     public int randomInt(int min, int max) {
         return (int)(Math.random() * (max - min + 1)) + min;
+    }
+
+    public void setDoor(String direction, int posX, int posY, int nextRoomPosX, int nextRoomPosY){
+        this.direction = direction;
+        this.posX = posX;
+        this.posY = posY;
+        this.nextRoomPosX = nextRoomPosX;
+        this.nextRoomPosY = nextRoomPosY;
     }
 }
